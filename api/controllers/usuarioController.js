a0bd8a1d43fe572760868225462aba051bc8bf5a
Samael Agustin <agustinsamael@gmail.com>
2022-10-29 21:20:51 -0600

    Se creó la vista para el listado de usuarios

--- api/controllers/usuarioController.js
+++ api/controllers/usuarioController.js
@@ -5,16 +5,19 @@ const Usuario = require('../../database/models/usuarios')
 // }
 
 class UsuarioController {
+
+    static async user (req, res) {
+        res.render('administrarUsuario');
+    }
+
     static async showAll (req, res) {
         const Usuarios = await Usuario.findAll({
             attributes: ['id_usuario', 'nombre_usuario', 'apellido_usuario', 'estado_usuario']
         });
-        console.log(Usuarios);
-        res.render('usuarioCreate', {
+        res.render('usuario/usuarioCreate', {
             usuarios : Usuarios
         });
     }
-
 }
 
 module.exports = UsuarioController
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -7,7 +7,8 @@ const UsuarioController = require('../controllers/usuarioController');
 router.get('/',loginController.login);
 router.post('/auth',loginController.auth);
 router.get('/logout',loginController.logout);
-router.get('/usuarios',UsuarioController.showAll);
+router.get('/usuarios',UsuarioController.user);
+router.get('/usuarios-index',UsuarioController.showAll);
 
 router.get('/dashboard',indexController.dashboard);
 
