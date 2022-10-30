ae9896b2588d1a7257e4716beaeaca80ff986798
Samael Agustin <agustinsamael@gmail.com>
2022-10-29 21:04:49 -0600

    Se creó el endpoint para listar todos los usuarios del sistema

--- api/controllers/usuarioController.js
+++ api/controllers/usuarioController.js
@@ -1,5 +1,20 @@
-var pool = require('../../database/conexion.js');
+const Usuario = require('../../database/models/usuarios')
 
-exports.user = (req, res) => {
-    res.render('usuarioCreate');
+// exports.user = (req, res) => {
+//     res.render('usuarioCreate');
+// }
+
+class UsuarioController {
+    static async showAll (req, res) {
+        const Usuarios = await Usuario.findAll({
+            attributes: ['id_usuario', 'nombre_usuario', 'apellido_usuario', 'estado_usuario']
+        });
+        console.log(Usuarios);
+        res.render('usuarioCreate', {
+            usuarios : Usuarios
+        });
+    }
+
 }
+
+module.exports = UsuarioController
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/loginController');
 const indexController = require('../controllers/indexController');
-const usuarioController = require('../controllers/usuarioController');
+const UsuarioController = require('../controllers/usuarioController');
 
 router.get('/',loginController.login);
 router.post('/auth',loginController.auth);
 router.get('/logout',loginController.logout);
-router.get('/usuarios',usuarioController.user);
+router.get('/usuarios',UsuarioController.showAll);
 
 router.get('/dashboard',indexController.dashboard);
 
