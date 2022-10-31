b23ba5ca95c883f19bd7045ba7670de2e35708a0
Samael Agustin <agustinsamael@gmail.com>
2022-10-31 00:05:55 -0600

    Creación de Crud del usuario

--- api/controllers/usuarioController.js
+++ api/controllers/usuarioController.js
@@ -5,6 +5,21 @@ const {Usuario} = require('../../database/models/index')
 // }
 
 class UsuarioController {
+
+    // redireccionar a la vista de crear usuario
+    static create (req, res) {
+        res.render('usuario/usuarioCreate');
+    }
+
+    static async edit (req, res) {
+        const usuario = await Usuario.findByPk(req.params.id);
+        console.log(usuario);
+        res.render('usuario/usuarioEdit', {
+            usuario : usuario.dataValues
+        });
+    }
+
+    // metodos de la clase
     static async showAll (req, res) {
         const Usuarios = await Usuario.findAll({
             attributes: ['id_usuario', 'nombre_usuario', 'apellido_usuario', 'estado_usuario']
@@ -12,6 +27,51 @@ class UsuarioController {
         res.render('usuario/usuarioIndex', {
             usuarios : Usuarios
         });
+    }
+
+    static async show (req, res) {
+        const usuario = await Usuario.findByPk(req.params.id);
+        res.render('usuario/usuarioShow', {
+            usuario
+        });
+    }
+
+    static async store (req, res) {
+        const { userName, lastName, password } = req.body;
+        if(!userName || !lastName || !password) {
+            // res.render('usuario/usuarioCreate', {
+            //     error: 'Campos incompletos'
+            // });
+            console.log('Campos incompletos');
+        }else {
+            console.log(req.body);
+            const usuario = await Usuario.create({
+                nombre_usuario: userName,
+                apellido_usuario: lastName,
+                password: password
+            })
+            res.redirect('/usuarios');
+        }
+    }
+
+    static async update (req, res) {
+        const usuario = await Usuario.update(req.body, {
+            where: {
+                id_usuario: req.params.id
+            }
+        });
+        res.redirect('/usuarios');
+    }
+
+    static async delete (req, res) {
+        console.log(req.params);
+
+        const usuario = await Usuario.destroy({
+            where: {
+                id_usuario: req.params.id
+            }
+        });
+        res.redirect('/usuarios');
     }
 }
 
--- api/routes/usuario.js
+++ api/routes/usuario.js
@@ -2,7 +2,13 @@ const Router = require('express').Router
 const router = Router()
 const UsuarioController = require('../controllers/usuarioController')
 
-router.get('/', UsuarioController.showAll)
-// router.get('/:id', UsuarioController.show)
+// redirecciones
+router.get('/crear', UsuarioController.create);
+router.get('/edit/:id', UsuarioController.edit);
+// metodos
+router.get('/', UsuarioController.showAll);
+router.post('/', UsuarioController.store);
+router.post('/editar/:id', UsuarioController.update);
+router.get('/eliminar/:id', UsuarioController.delete);
 
 module.exports = router
